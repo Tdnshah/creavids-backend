168caca6140f1eeaa0c95bcc4c98054ab96eb630
tdnshah <sanketejas@gmail.com>
2020-06-18 20:38:22 +0530

    CreavidsBlog-002: Database.js updated to use .env file.

--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'creavids-blog'),
-        username: env('DATABASE_USERNAME', 'creavids-blog'),
-        password: env('DATABASE_PASSWORD', 'creavids-blog'),
-        ssl: env.bool('DATABASE_SSL', false),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: env.bool('DATABASE_SSL'),
       },
       options: {}
     },
